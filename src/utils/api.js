fbe50920b3d6bfbb3e0d154e8d7aa21d38f7a83e
Siddesh s k <siddusiddesh551@gmail.com>
2025-11-19 11:19:12 +0530

    url updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE = process.env.REACT_APP_API_URL || 'https://smartinterview-backend-jer1.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE,
